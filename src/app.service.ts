082ba29ce4492475b15227c08ad3130c85327921
tarinaihitori <kazuro0530@gmail.com>
2025-09-28 18:32:20 +0000

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const a = 2;
+    const a = 3;
     console.log(a);
     return 'Hello World!';
   }
